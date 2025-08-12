46ae98da6055da317517757870fee90b58c231c1
srinivasbadiga2808 <srinivasbadiga2808@gmail.com>
2025-08-12 14:59:26 -0400

    Create ProductRepository.java

---
+++ src/main/java/com/productservice/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.example.productservice.repository;
+
+import com.example.productservice.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+  Optional<Product> findBySku(String sku);
+  boolean existsBySku(String sku);
+}
